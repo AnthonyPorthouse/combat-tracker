bb2f0f3bb4bc05190d8a6284348e50ca97dce866
Anthony Porthouse <anthony.porthouse@partnerize.com>
2019-09-09 16:31:06 +0100

    Relative paths

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -36,7 +36,7 @@ module.exports = {
   env: {
     NODE_ENV: {
       production: {
-        use: ['@neutrinojs/pwa', {relativePaths: true}]
+        use: ['@neutrinojs/pwa', {relativePaths: true, publicPath: '/combat-tracker/'}]
       }
     }
   }
