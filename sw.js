ee2977980f947accfdcc6ef00107320bea46d753
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-09 17:20:51 +0000

    Deploy AnthonyPorthouse/combat-tracker to github.com/AnthonyPorthouse/combat-tracker.git:gh-pages

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 var __wpo = {
   "assets": {
     "main": [
-      "/index.d08ab3406bed19db9986.js",
+      "/index.f84bcef9ee480dc8ab54.js",
       "/runtime.a9edc78d5cd6127c9851.js",
       "/index.bb01ad375a723c6b759ae15c7dd7f03e.css",
       "/",
@@ -14,17 +14,17 @@ var __wpo = {
   },
   "externals": [],
   "hashesMap": {
-    "0903fcd2b3b507d66dbccf2c995e57c56a986403": "/index.d08ab3406bed19db9986.js",
+    "0903fcd2b3b507d66dbccf2c995e57c56a986403": "/index.f84bcef9ee480dc8ab54.js",
     "dc853b08f6c5bc70f64578cdd2a66cf301d1dcd9": "/runtime.a9edc78d5cd6127c9851.js",
     "602146bf6fbcf7e0bb64f850bdad466d479bfc35": "/index.bb01ad375a723c6b759ae15c7dd7f03e.css",
-    "e8cc04217756c5d557648d7e5279a12bc13c67dc": "/",
+    "004e4369caaa195ff4fdb446a8c5733b11712187": "/",
     "741a42113f39c9bb9663ad88cec3251d718b81e3": "/static/icon-256.png",
     "ed711bde262a495c494f2c646d36df001a436946": "/static/icon-512.png",
     "37389d49488050b7a87e1d2c8890707012d79e2d": "/static/manifest.webmanifest"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "9/9/2019, 3:32:31 PM",
+  "version": "9/9/2019, 5:20:40 PM",
   "name": "webpack-offline",
   "pluginVersion": "4.9.1",
   "relativePaths": false
