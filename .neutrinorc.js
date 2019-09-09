3df0ae64e8ffbc09403cfe27510f0f2b799280f9
Anthony Porthouse <anthony.porthouse@partnerize.com>
2019-09-09 15:59:30 +0100

    Add initial manifest

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -5,25 +5,32 @@ module.exports = {
       '@neutrinojs/react',
       {
         html: {
-          title: 'Combat Tracker'
+          title: 'Combat Tracker',
+          links: [
+            { href: "/static/manifest.webmanifest", rel: "manifest"},
+            { href: "/static/icon-256.png", rel: "shortcut icon"},
+            { href: "/static/icon-256.png", rel: "apple-touch-icon"},
+          ]
         }
       }
     ],
-    ['@neutrinojs/style-loader', {
-      loaders: [
-        {
-          loader: 'postcss-loader',
-          options: {
-            plugins: [require('tailwindcss'), require('autoprefixer')]
+    [
+      '@neutrinojs/style-loader',
+      {
+        loaders: [
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [require('tailwindcss'), require('autoprefixer')]
+            }
           }
-        }
-      ]
-    },
+        ]
+      },
     ],
     '@neutrinojs/jest',
     (neutrino) => {
       neutrino.config.resolve.alias.store.set('react-dom', '@hot-loader/react-dom');
-    }
+    },
   ],
 
   env: {
